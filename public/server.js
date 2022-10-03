d2ae565da65be854b7d27719e3e1ed5e1c49ddf2
axelhagerback <axel.hagerback@gmail.com>
2022-10-03 15:24:50 +0200

    server.js writes out html file

--- public/server.js
+++ public/server.js
@@ -1,14 +1,19 @@
 var http = require('http');
+var fs = require('fs');
 
 var hostname ='127.0.0.1';
 var port = 8080;
 
 var server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    res.end('<h1>Hello from Node</h1>');
+    fs.readFile('index.html', 'utf8', function(err, data){
+        console.log(data);
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/html');
+        res.write(data);
+        res.end();
+    });
 });
 
 server.listen(port, hostname, () => {
-    console.log('server is running');
+    console.log('Server is running');
 });
